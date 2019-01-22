289deae54d2037ac2a48eefdb5d27e27bc25a594
Róbert Tóth <robert.toth.ext@nokia.com>
2019-01-22 16:18:57 +0100

    promise, async

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const {app, BrowserWindow} = require('electron') 
-const url = require('url') 
-const path = require('path')  
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
 var MongoClient = require('mongodb').MongoClient;
 const MongoDbUrl = "mongodb://localhost:27017/testdb"
-let win  
+let win;
+var db = MongoClient.connect(MongoDbUrl, { useNewUrlParser: true });
 
-function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 600}) 
-   win.loadURL(url.format ({ 
-      pathname: path.join(__dirname, 'index.html'), 
-      protocol: 'file:', 
-      slashes: true 
+function createWindow() {
+   win = new BrowserWindow({ width: 800, height: 600 })
+   win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
    }));
 
    // const db = await MongoClient.connect(MongoDbUrl)
@@ -20,21 +21,64 @@ function createWindow() {
    //       console.log(result);
    //       });
    // } catch (error) {
-      
-   // }
-   MongoClient.connect(MongoDbUrl, function(err, client) {
-      if(!err) {
-          var db = client.db("testdb")  
-          console.log("We are connected");
-          var collection = db.collection('monsters');
-          var oneMonster = collection.findOne({"name" : "Aboleth"}, function(err, result){
-              console.log(result);
-          });
-          
-      }
-      });
 
-   
-}  
+   // }
+
+   var attrMod = (attr) => Math.floor((attr - 10) / 2);
+
+   function retrieveCreature(cName, callback) {
+      db.then(
+         client => {
+            const db = client.db("testdb");
+            console.log("We are connected!");
+            const collMonsters = db.collection('monsters');
+
+            collMonsters.findOne({ "name": cName }, (err, creature) => {
+               callback(creature);
+            });
+         }
+      )
+   }
+
+
+   var retrieveImprov = (cType, callback) => {
+
+      db.then(
+         client => {
+            const db = client.db("testdb");
+            const collImprovs = db.collection('improvement');
+
+            collImprovs.findOne({ "type": cType }, (err, impr) => {
+
+               callback(impr);
+            });
+         }
+      );
+   };
+
+   retrieveCreature("Aboleth", (creature) => {
+      creature.hitPoints = creature.hitDice.avgHitPoints;
+      creature.strMod = () => attrMod(creature.str);
+      creature.dexMod = () => attrMod(creature.dex);
+      creature.conMod = () => attrMod(creature.con);
+      creature.intMod = () => attrMod(creature.int);
+      creature.wisMod = () => attrMod(creature.wis);
+      creature.charMod = () => attrMod(creature.char);
+      //    creature.baseAttackModifier = function(){
+      //       let lookup = creatureImprovement.find(function(impr){
+      //           return impr.type == creature.type;
+      //       });
+      //       return Math.floor(creature.hitDice.numOfHitDice * lookup.attackBonus);
+      //   }
+      //    creature.attackModifierMelee = creature.baseAttackModifier() + creature.abilities[0].modifier + sizeModifiers[5].sizeModifier;
+      console.log(creature.strMod());
+   });
+
+
+   retrieveImprov("Élőhalott", improv => {
+      console.log(improv);
+   });
+
+}
 
 app.on('ready', createWindow) 
